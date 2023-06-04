177ae0b1594df613829be7540d7b04518a9b7ad9
Gyeomyi Hwang <austin.hwang.dev@gmail.com>
2023-06-04 14:50:25 +0900

    Feat: Framer motion 초기 세팅 및 첫 애니메이션 구현
    - Dependency 설치
    - Framer motion + Styled-components
    - Framer motion component에 첫 애니메이션 구현

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { motion, spring } from "framer-motion";
+const Wrapper = styled.div`
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Box = styled(motion.div)`
+	width: 200px;
+	height: 200px;
+	background-color: white;
+	border-radius: 15px;
+	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
+	return (
+		<Wrapper>
+			<Box
+				// initial condition
+				initial={{ scale: 0 }}
+				animate={{ scale: 1, rotateZ: 360 }}
+				transition={{ type: "tween", delay: 0.5, duration: 2 }}
+			/>
+		</Wrapper>
+	);
+}
+
+export default App;
