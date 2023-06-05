d26ff15974fbdfb3208c62cf4665457e9c37777a
Gyeomyi Hwang <austin.hwang.dev@gmail.com>
2023-06-05 14:24:14 +0900

    Feat: MotionValue(1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { motion, Variants } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue, Variants } from "framer-motion";
+import { useEffect } from "react";
 const Wrapper = styled.div`
 	height: 100vh;
 	width: 100vw;
@@ -28,23 +28,23 @@ const Box = styled(motion.div)`
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants: Variants = {
-	click: { scale: 1, borderRadius: "100px" },
-};
-
 function App() {
-	const biggerBoxRef = useRef<HTMLDivElement>(null);
+	const x = useMotionValue(0);
+	// 현재 MotionValue값 출력
+	useEffect(() => {
+		x.onChange(() => console.log(x.get()));
+	}, [x]);
 	return (
 		<Wrapper>
-			<BiggerBox ref={biggerBoxRef}>
-				<Box
-					variants={boxVariants}
-					drag
-					dragElastic={1}
-					dragConstraints={biggerBoxRef}
-					whileTap={"click"}
-				/>
-			</BiggerBox>
+			<button
+				onClick={() => {
+					// MotionValue값 업데이트
+					x.set(200);
+				}}
+			>
+				Update!
+			</button>
+			<Box style={{ x: x }} drag="x" dragSnapToOrigin />
 		</Wrapper>
 	);
 }
