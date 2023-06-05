3b28f740129fee715c11f2c93da7ef8e922de4d3
Gyeomyi Hwang <austin.hwang.dev@gmail.com>
2023-06-05 10:39:53 +0900

    Feat: whileHover, whileTap

--- src/App.tsx
+++ src/App.tsx
@@ -9,65 +9,27 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-	display: grid;
-	grid-template-columns: repeat(2, 1fr);
 	width: 200px;
 	height: 200px;
-	background-color: rgba(255, 255, 255, 0.2);
+	background-color: rgba(255, 255, 255, 1);
 	border-radius: 40px;
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-	background-color: white;
-	place-self: center;
-	height: 70px;
-	width: 70px;
-	border-radius: 35px;
-	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants: Variants = {
-	start: { scale: 0.5, opacity: 0 },
-	end: {
-		scale: 1,
-		opacity: 1,
-		transition: {
-			type: "spring",
-			duration: 1,
-			bounce: 0.5,
-			// 자식 컴포넌트에 transition: {delay:  0.5} 부여 - 1번째 자식은 staggerChildren이 적용되지 않기 때문에 delayChildren으로 전체 자식들 0.5초씩 딜레이 추가
-			delayChildren: 0.5,
-			staggerChildren: 0.5,
-		},
-	},
-};
-
-const circleVariants: Variants = {
-	start: {
-		opacity: 0,
-		// 애니메이션 실행 전 initial y축 좌표
-		y: 10,
-	},
-	end: {
-		opacity: 1,
-		// 애니메이션 실행 후 종료되는 y축 좌표
-		y: 0,
-		transition: { duration: 1 },
-	},
+	hover: { scale: 1.5, rotateZ: 90 },
+	click: { scale: 1, borderRadius: "100px" },
 };
 
 function App() {
 	return (
 		<Wrapper>
-			<Box variants={boxVariants} initial="start" animate="end">
-				<Circle variants={circleVariants} />
-				<Circle variants={circleVariants} />
-				<Circle variants={circleVariants} />
-				<Circle variants={circleVariants} />
-			</Box>
+			<Box variants={boxVariants} whileHover={"hover"} whileTap={"click"} />
 		</Wrapper>
 	);
 }
 
 export default App;
+
+// whileHover: hover 이벤트 시
+// whileTap: 클릭중일 시
