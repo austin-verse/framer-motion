182100eddbc0fbba83e8ce1f65b40724aacf2311
Gyeomyi Hwang <austin.hwang.dev@gmail.com>
2023-06-06 21:08:11 +0900

    Slider(1)

--- src/App.tsx
+++ src/App.tsx
@@ -13,56 +13,70 @@ const Wrapper = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
-	width: 200px;
-	height: 200px;
+	position: absolute;
+	top: 200px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 28px;
+	font-size: 700;
+	width: 100px;
+	height: 100px;
 	background-color: rgba(255, 255, 255, 1);
 	border-radius: 40px;
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants: Variants = {
-	initial: { opacity: 0, scale: 0 },
-	visible: { opacity: 1, scale: 1, rotateZ: 360 },
-	leaving: { opacity: 0, scale: 0, y: 50 },
+	invisible: {
+		x: 500,
+		opacity: 0,
+		scale: 0,
+	},
+	visible: {
+		x: 0,
+		opacity: 1,
+		scale: 1,
+		transition: {
+			duration: 1,
+		},
+	},
+	exit: {
+		x: -500,
+		opacity: 0,
+		scale: 0,
+		transition: {
+			duration: 1,
+		},
+	},
 };
 
 function App() {
-	const [showing, setShowing] = useState(false);
-	const showingHandler = () => {
-		setShowing(!showing);
-	};
+	const [visible, setVisible] = useState(1);
+	const nextSlideHandler = () =>
+		setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+	const prevSlideHandler = () =>
+		setVisible((prev) => (prev === 1 ? 1 : prev - 1));
 	return (
 		<Wrapper>
 			<AnimatePresence>
-				{showing ? (
-					<Box
-						variants={boxVariants}
-						initial="initial"
-						animate="visible"
-						// 박스가 사라질 때 하는 행동
-						exit="leaving"
-					/>
-				) : null}
+				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(function (a, i) {
+					return a === visible ? (
+						<Box
+							variants={boxVariants}
+							initial="invisible"
+							animate="visible"
+							exit="exit"
+							key={a}
+						>
+							{a}
+						</Box>
+					) : null;
+				})}
 			</AnimatePresence>
-			<button
-				style={{ position: "absolute", marginTop: 300 }}
-				onClick={showingHandler}
-			>
-				Click me
-			</button>
+			<button onClick={prevSlideHandler}>prev</button>
+			<button onClick={nextSlideHandler}>next</button>
 		</Wrapper>
 	);
 }
 export default App;
-
-// AnimatePresence
-// ReactJS어플에서 사라지는 콤퍼넌트를 에니메이션함
-// 리액트 트리에서 컴포넌트가 제거될 때 제거되는 컴포넌트에 애니메이션 효과를 줄 수 있음
-// 컴포넌트가 마운트되거나 언마운트될 때 애니메이션을 처리,ㅣ
-
-// Box가 사라질 때 Box를 animate
-
-// AnimatePresence는 visible상태여야함 - AnimatePresence의 내부에는 조건문이 존재하여야함
-// AP는 자신의 안에 나타나거나 사라지는 것이 있다면 그것을 animate할 수 있도록 해줌
-
-// exit : 이 element가 사라질 때 어떤 애니메이션을 발생시키는 지를 정함
